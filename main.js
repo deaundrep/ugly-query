e983e755c522cbcb684a534ff671ac815d7ae4d0
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-02-11 09:55:50 -0500

    done

---
+++ main.js
@@ -0,0 +1,32 @@
+const newText = document.querySelector('p');
+newText.style.color = 'red';
+newText.style.fontSize = '50px'
+
+const titleText = document.querySelector('h1');
+titleText.style.color = 'green';
+titleText.style.fontsize = '2em';
+
+const someLorem = document.querySelector('h3');
+someLorem.style.color = 'blue';
+someLorem.style.fontSize = '4em';
+
+document.body.style.backgroundColor = "purple";
+
+const image = document.querySelector.apply('img');
+
+image.src = 'https://static01.nyt.com/images/2019/10/06/arts/06jokers-ranked-ledger/06jokers-ranked-ledger-superJumbo.jpg?quality=90&auto=webp'
+image.style.height = '300px';
+
+const someday = document.querySelector('textarea');
+someday.style.color = 'yellow';
+someday.style.fontSize = '5em';
+
+
+
+
+
+
+
+
+
+
